d32c437fdc28e616ff2370886c7a54216d0bbc59
Daniel Soares <dlsoares18@gmail.com>
2023-12-16 13:05:46 +0000

    primeiros envios

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Users</a>
+            </li>
+            <li>
+              <a href="/lista">Lista Factos</a>
+            </li>
+            <li>
+              <a href="/nome">Nome</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
