2c5e424b811c66873bacbdccebdbd93a1b43e7aa
Thales Alves <thalesguimaraes@gmail.com>
2023-12-27 16:23:27 +0000

    add login behavior and store token

--- src/components/loginForm/loginForm.js
+++ src/components/loginForm/loginForm.js
@@ -1,77 +1,125 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "./loginForm.css";
-
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 function LoginForm() {
 
-   const [postData, setPostData] = useState({
+    const [postData, setPostData] = useState({
         username: '',
         password: '',
-     });
-   
-   const handleInputChange = (e) => {
-       const { name, value } = e.target;
-       setPostData((prevData) => ({
-         ...prevData,
-         [name]: value,
-       }));
-   };
-   
-   const handlePostRequest = async () => {
-       try {
-         const response = await axios.post('https://localhost:7271/api/Authenticate/login', postData);
-         console.log('Resposta da API:', response.data);
-       } catch (error) {
-         console.error('Erro ao fazer a solicitação POST:', error);
-         // Adicione lógica para lidar com o erro, se necessário
-       }
-   };
-   
-   return (
-       <div className="container">
+    });
+
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [showToast, setShowToast] = useState(false);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setPostData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    useEffect(() => {
+        if (location.state && location.state.showToast) {
+            setShowToast(true);
+        }
+    }, [location]);
+
+    useEffect(() => {
+        if (showToast) {
+            toast.error("Login failed. Please try again.");
+            setShowToast(false);
+        }
+    }, [showToast]);
+
+    const handlePostRequest = async () => {
+        try {
+            const response = await axios.post('https://localhost:7271/api/Authenticate/login', postData);
+            console.log('Resposta da API:', response.data);
+
+            if (response.status === 200) {
+                localStorage.setItem('token', response.data.token);
+
+
+                /*   TODO:
+
+                    - later to logout use: 
+                        localStorage.removeItem('token');
+
+                    - To get the token and send to the backend:
+                        const token = localStorage.getItem('token');
+        
+                        axios.get('/protected-route', {
+                        headers: {
+                        Authorization: `Bearer ${token}`
+                        }
+                        }); 
+                        
+                */
+
+
+                navigate('/');
+            }
+
+        } catch (error) {
+            console.error('Erro ao fazer a solicitação POST:', error);
+            navigate('/login', { state: { showToast: true } });
+        }
+    };
+
+    return (
+
+        <div className="container">
             <header className="header">
                 <span>Entre na sua Conta</span>
             </header>
 
-            <form>       
+            <form>
                 <div className="inputContainer">
-                    <label htmlFor="email">Email</label>                
-                    <input 
-                        type="email" 
-                        name="username" 
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="username"
                         id="username"
-                        value={postData.username} 
+                        value={postData.username}
                         onChange={handleInputChange}
-                        required 
+                        required
                     />
                 </div>
 
                 <div className="inputContainer">
-                    <label htmlFor="password">Senha</label>                
-                    <input 
-                        type="password" 
-                        name="password" 
+                    <label htmlFor="password">Senha</label>
+                    <input
+                        type="password"
+                        name="password"
                         id="password"
-                        value={postData.password} 
+                        value={postData.password}
                         onChange={handleInputChange}
                         /*pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,}$"*/
                         title="A senha deve ter pelo menos 6 caracteres, pelo menos um caracter não alfanumérico, pelo menos um caractere numérico, pelo menos um caracter em maiúsculas e pelo menos um caracter em minúsculas"
-                        required 
+                        required
                     />
                 </div>
 
                 <button type="button" onClick={handlePostRequest}>
                     Entrar
-                </button>            
+                </button>
             </form>
+
+            <ToastContainer />
 
             <div className='footer'>
                 <p>Não tem uma conta? <a href='/register'>Cadastre-se</a> </p>
             </div>
-       </div>
-     );
-   };
+        </div>
+    );
+};
 
 
 export default LoginForm;
